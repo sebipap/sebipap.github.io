e4dffca745324b108132292b7662bb15bec2007e
Sebastián Papanicolau <37161571+sebipap@users.noreply.github.com>
2021-03-20 17:12:44 -0300

    2 commit
    desc

---
+++ precache-manifest.f57ad8af7a020a594735e203d124345e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f5408c7614e12316ea048747f175a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3aecb68e25c3d6e8431",
+    "url": "/static/css/main.a312c9cb.chunk.css"
+  },
+  {
+    "revision": "cc236798b77f40ad291c",
+    "url": "/static/js/2.ff55b09e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff55b09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3aecb68e25c3d6e8431",
+    "url": "/static/js/main.a95597aa.chunk.js"
+  },
+  {
+    "revision": "1977821e1ef12fc99b56",
+    "url": "/static/js/runtime-main.a2e7fedb.js"
+  },
+  {
+    "revision": "e32496172e8271f039b691350e59c971",
+    "url": "/static/media/bg.e3249617.jpg"
+  }
+]);
